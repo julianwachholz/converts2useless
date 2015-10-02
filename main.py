62142d94617722e7c364520c2eebb8779ef69fe8
Julian Wachholz <julian@wachholz.ch>
2015-10-02 08:21:09 +0200

    Use new RedditBot classes from main

--- main.py
+++ main.py
@@ -1,52 +1,24 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-import collections
 import logging
-import random
-import time
-
-from datetime import datetime, timedelta
-from itertools import cycle
-
-import praw
+import json
+import sys
 
 from useless import get_units, convert_useless
 
-
-FORMAT = '%(asctime)-15s %(levelname)-8s %(name)-10.10s %(message)s'
-logging.basicConfig(level=logging.INFO, format=FORMAT)
-logger = logging.getLogger('converts2useless')
+from convert_bot import ConvertBot
 
 
-USER_AGENT = 'Converts2Useless v1.1.2 (by /u/yousai)'
+logger = logging.getLogger('main')
 
-CLIENT_ID = '***REMOVED***'
-CLIENT_SECRET = '***REMOVED***'
-REDIRECT_URI = 'http://127.0.0.1:65010/authorize_callback'
 
-SCOPE = 'identity read submit edit privatemessages'
-
-BOT_NAME = 'Converts2Useless'
 CAPS_SUBS = [  # reply ALL CAPS in these subreddits
     'totallynotrobots',
 ]
 
-ACCESS_INFO = {
-    'access_token': '***REMOVED***',
-    'scope': set(SCOPE.split()),
-    'refresh_token': '***REMOVED***',
-}
-
-r = praw.Reddit(user_agent=USER_AGENT)
 
 
-LIMIT = 50
-MAX_AGE = timedelta(minutes=15)
-MAX_DEPTH_CHECK = 4  # check scores on X parent comments
-MAX_REPLIES_PER_POST = 3
-WAIT_AFTER = 60  # how long to wait after posting a comment
-
-SUBMISSIONS = collections.Counter()
+# SUBMISSIONS = collections.Counter()
 
 
 REPLY_TEXTS = [
@@ -60,37 +32,6 @@ REPLY_TEXTS = [
     u'{value} is the same as {original}',
 ]
 REPLY_INFO = u' ^[[BotInfo]](/r/Converts2Useless)'
-
-
-def check_comment(comment, blacklist):
-    now = datetime.utcnow()
-    created = datetime.utcfromtimestamp(comment.created_utc)
-
-    if SUBMISSIONS[comment.link_id] >= MAX_REPLIES_PER_POST:
-        logger.info('submission cap on thread %s reached' % comment.submission.fullname)
-        return False
-
-    if created + MAX_AGE < now:
-        logger.debug('%s is older than 15min (now: %s)' % (created, now))
-        return False
-
-    if not comment.author or comment.author.name in blacklist:
-        logger.info('/u/%s is on blacklist' % comment.author.name)
-        return False
-    return True
-
-def _check_comment_score(comment, depth=0):
-    """Check if any parent comment has a negative score."""
-    if comment.author.name == BOT_NAME:
-        return False  # dont participate in reply chains
-    if not comment.score_hidden and comment.score < 1:
-        return False
-    if comment.is_root or depth > MAX_DEPTH_CHECK:
-        return True
-    global r
-    logger.info('checking score on %s: depth %d' % (comment.parent_id, depth))
-    parent = r.get_info(thing_id=comment.parent_id)
-    return _check_comment_score(parent, depth + 1)
 
 
 def reply_comment(comment):
@@ -145,34 +86,10 @@ def get_reddit():
     r.set_oauth_app_info(client_id=CLIENT_ID,
                          client_secret=CLIENT_SECRET,
                          redirect_uri=REDIRECT_URI)
-    r.set_access_credentials(**ACCESS_INFO)
+    # r.set_access_credentials(**ACCESS_INFO)
 
     # url = r.get_authorize_url('uniqueKey', SCOPE, True)
     # access_info = r.get_access_information('***REMOVED***')
-    logger.info('Logged in as %s (%s)' % (r.user.name, r.user.id))
-
-
-def get_comments(sub, blacklist, before=None):
-    """
-    Always remember ID of last submitted comment, so we don't
-    have to save anything else to keep running and not do the
-    same work twice.
-
-    """
-    logger.debug('newest %d comments from /r/%s after %r'
-                % (LIMIT, sub.display_name, before))
-    latest_created = 0
-    params = {
-        'before': before,
-        'sort': 'old',
-    }
-    for comment in sub.get_comments(limit=LIMIT, params=params):
-        if comment.created_utc > latest_created:
-            latest_created = comment.created_utc
-            before = comment.fullname
-        if check_comment(comment, blacklist) and reply_comment(comment):
-            return comment.fullname
-    return before
 
 
 START_TEXT = u'hey %s! you are now whitelisted for me! yay!'
@@ -230,53 +147,17 @@ def check_mail(blacklist, whitelist):
         return True
 
 
-def read_lists():
-    with open('blacklist.txt') as f:
-        blacklist = set(map(lambda s: s.strip(), f.readlines()))
-        logger.info('updated blacklist: %d entries' % len(blacklist))
-    with open('whitelist.txt') as f:
-        subreddits = set(map(lambda s: s.strip(), f.readlines()))
-        logger.info('updated whitelist: %d entries' % len(subreddits))
-    return blacklist, subreddits
-
-
-def write_lists(blacklist, whitelist):
-    with open('blacklist.txt', 'w') as f:
-        f.write('\n'.join(blacklist))
-    with open('whitelist.txt', 'w') as f:
-        f.write('\n'.join(whitelist))
-
-
-def loop(blacklist, subs):
-    global r
-    last_fullnames = {}
-
-    logger.info('looping %r' % subs)
-
-    try:
-        for sub_name in cycle(subs):
-            logger.debug('cycle: %s' % sub_name)
-            latest = last_fullnames.get(sub_name, None)
-            last_fullnames[sub_name] = get_comments(r.get_subreddit(sub_name), blacklist, before=latest)
-            if check_mail(blacklist, subs):
-                break
-            time.sleep(2)
-    except praw.errors.Forbidden:
-        # bot got a 403 when trying to reply, remove subreddit from whitelist
-        logger.error('Forbidden in /r/%s' % sub_name)
-        subs.remove(sub_name)
-        write_lists(blacklist, subs)
-
-
-def main():
-    get_reddit()
-
-    while True:
-        # loop only breaks if subreddits needs updating
-        blacklist, subreddits = read_lists()
-        loop(blacklist, subreddits)
-        logger.info('loop refreshed')
-
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 2:
+        sys.stderr.write('Usage: {} <config_file>'.format(sys.argv[0]))
+        sys.exit(1)
+
+    with open(sys.argv[1]) as config_file:
+        config = json.load(config_file)
+
+    LOG_FORMAT = '%(asctime)-15s %(levelname)-6s %(name)-15.15s %(message)s'
+    logging.basicConfig(level=config.get('loglevel', 'WARN'), format=LOG_FORMAT)
+
+    bot = ConvertBot(config)
+    bot.start()
